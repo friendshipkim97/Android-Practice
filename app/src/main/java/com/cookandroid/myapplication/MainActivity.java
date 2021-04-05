55350b8861de7de444c111020b7bd7fdb570d126
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-04-05 15:38:57 +0900

    update

--- app/src/main/java/com/cookandroid/myapplication/MainActivity.java
+++ app/src/main/java/com/cookandroid/myapplication/MainActivity.java
@@ -1,14 +1,21 @@
 package com.cookandroid.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,33 +26,25 @@ public class MainActivity extends AppCompatActivity {
     private Button btn_move;
     private EditText et_test;
     private String str;
+    private ListView list;
+     EditText et_save;
+     String shared = "file";
+
+    private SwitchCompat sc_save;
+    private String sharedSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_test = findViewById(R.id.et_test);
+        init();
 
-
-        btn_move = findViewById(R.id.btn_move);
-        btn_move.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                  Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                  str = et_test.getText().toString();
-                  intent.putExtra("str", str);
-                  startActivity(intent); // 액티비티 이동
-            }
-        });
-
-        test = (ImageView)findViewById(R.id.test);
-        test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "김정우짱", Toast.LENGTH_SHORT).show();
-            }
-        });
+        buttonMove();
+        clickImage();
+        adapt();
+        textSharedPreferences();
+        switchSharedPreferences();
 
 //        et_id = findViewById(R.id.et_id);
 //        btn_test = findViewById(R.id.btn_test);
@@ -56,6 +55,76 @@ public class MainActivity extends AppCompatActivity {
 //                et_id.setText("김정우");
 //            }
 //        });
+    }
+
+    private void switchSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences(sharedSwitch, 0);
+        Boolean checked = sharedPreferences.getBoolean("kim", false); // 아무 값도 들어있지 않으면 false를 반환한다.
+        sc_save.setChecked(checked);
+    }
+
+    private void textSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        String value = sharedPreferences.getString("jung", ""); // 아무 값도 들어 있지 않으면 ""를 반환하는 것
+        System.out.println("값체크"+value);
+        et_save.setText(value);
+    }
+
+    @Override
+    protected void onDestroy() { // 앱을 종료를 시켰을 때, 엑티비티를 벗어났을때, onDestroy가 호출된다.
+        super.onDestroy();
+
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String value = et_save.getText().toString(); // 현재 et_save에 써져 있는 값을 가져온다.
+        editor.putString("jung", value);
+        editor.commit(); // 실제적인 세이브
+
+        SharedPreferences sharedPreferences2 = getSharedPreferences(sharedSwitch, 0);
+        SharedPreferences.Editor editor2 = sharedPreferences2.edit();
+        Boolean checked = sc_save.isChecked();
+        editor2.putBoolean("kim", checked);
+        editor2.commit(); // 실제적인 세이브
+    }
+
+    private void adapt() {
+        List<String> data = new ArrayList<>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+        data.add("정우");
+        data.add("안드로이드");
+        data.add("사과");
+        adapter.notifyDataSetChanged();
+    }
+
+    private void init() {
+        et_test = findViewById(R.id.et_test);
+        btn_move = findViewById(R.id.btn_move);
+        list = (ListView)findViewById(R.id.list);
+        test = (ImageView)findViewById(R.id.test);
+        et_save = (EditText)findViewById(R.id.et_save);
+        sc_save =(SwitchCompat)findViewById(R.id.sc_save);
+    }
+
+    private void buttonMove() {
+        btn_move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                  str = et_test.getText().toString();
+                  intent.putExtra("str", str);
+                  startActivity(intent); // 액티비티 이동
+            }
+        });
+    }
+
+    private void clickImage() {
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "김정우짱", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
--- app/src/main/java/com/cookandroid/myapplication/SubActivity.java
+++ app/src/main/java/com/cookandroid/myapplication/SubActivity.java
@@ -4,22 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
 
     private TextView tv_sub;
+    public Button navigateMoveBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
-        tv_sub = findViewById(R.id.tv_sub);
+        init();
+        navigateMove();
 
         Intent intent = getIntent(); // 어디서 날라오는 데이터 값이 있으면 이걸로 받는다.
         String str = intent.getStringExtra("str"); // 데이터를 보낼때와 받는 이름이 같아야 한다.
-
         tv_sub.setText(str);
+
     }
+
+
+    private void init(){
+        tv_sub = findViewById(R.id.tv_sub);
+        navigateMoveBtn = findViewById(R.id.navigateMoveBtn);
+    }
+
+    private void navigateMove() {
+        navigateMoveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubActivity.this, NavigateMenu.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+//    private void buttonMove() {
+//        btn_move.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+//                str = et_test.getText().toString();
+//                intent.putExtra("str", str);
+//                startActivity(intent); // 액티비티 이동
+//            }
+//        });
+//    }
 }
