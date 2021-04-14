4f363a3a500f7405da4938c8cf9097d4e42a910d
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-04-14 12:02:11 +0900

    Update LinearLayout

---
+++ app/src/main/java/com/cookandroid/myapplication/LinearLayoutExam.java
@@ -0,0 +1,14 @@
+package com.cookandroid.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearLayoutExam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout_exam);
+    }
+}
