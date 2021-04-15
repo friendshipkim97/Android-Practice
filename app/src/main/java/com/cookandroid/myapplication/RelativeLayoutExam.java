a0fb642fe579cf6b08f8b62b7ad80930a38744ab
friendshipkim97 <61726631+friendshipkim97@users.noreply.github.com>
2021-04-15 18:23:28 +0900

    Update RelativeLayout

---
+++ app/src/main/java/com/cookandroid/myapplication/RelativeLayoutExam.java
@@ -0,0 +1,14 @@
+package com.cookandroid.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RelativeLayoutExam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_exam);
+    }
+}
